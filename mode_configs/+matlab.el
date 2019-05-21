97423d10b6744e43bcf219a87bd7293a52186e41
David R Connell <davidconnell12@gmail.com>
2019-05-20 19:47:14 -0500

    Add method needed for matlab-flychecker

--- mode_configs/+matlab.el
+++ mode_configs/+matlab.el
@@ -1,12 +1,16 @@
-(require 'matlab-mode)
-(setq matlab-server-executable "/usr/local/MATLAB/R2019a/bin/matlab")
-(matlab-mode-common-setup)
-(setq matlab-shell-command "/usr/local/MATLAB/R2019a/bin/matlab")
-(setq matlab-shell-command-switches (list "-nodesktop" "-nosplash"))
-
 (defcustom default-fill-column 80
   "Dummy var so matlab-mode works"
   :type 'integer)
+
+(defun string-to-int (str)
+  "So matlab-mode works"
+  (string-to-number str))
+
+(require 'matlab-mode)
+(setq matlab-server-executable "/usr/local/MATLAB/bin/matlab")
+(matlab-mode-common-setup)
+(setq matlab-shell-command "/usr/local/MATLAB/bin/matlab")
+(setq matlab-shell-command-switches (list "-nodesktop" "-nosplash"))
 
 (add-hook! 'matlab-mode-hook
   '(display-line-numbers-mode
