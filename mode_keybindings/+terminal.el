639027e77dc327f4946b64e297275b08795f1346
David R Connell <davidconnell12@gmail.com>
2019-07-10 11:20:16 -0500

    Get out of command line.
    Key-binding for moving out of command line to scroll through matlab repl
    output.

--- mode_keybindings/+terminal.el
+++ mode_keybindings/+terminal.el
@@ -10,7 +10,7 @@
           (map! :mode matlab-shell-mode
                 :n "k" #'matlab-shell-previous-matching-input-from-input
                 :n "j" #'matlab-shell-next-matching-input-from-input
+                :ni "C-k" #'previous-line
                 :i "C-p" #'matlab-shell-previous-matching-input-from-input
                 :i "C-n" #'matlab-shell-next-matching-input-from-input
-                :ni "C-d" #'matlab-shell-exit
-                :i [?\t] #'matlab-shell-tab))
+                :ni "C-d" #'matlab-shell-exit))
