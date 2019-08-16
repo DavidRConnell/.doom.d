ec5168d85177eb5144bf597d9ad04b35f3c3c910
David R Connell <davidconnell12@gmail.com>
2019-08-16 15:47:28 -0500

    Add keybindings

--- mode_keybindings/+org.el
+++ mode_keybindings/+org.el
@@ -3,5 +3,7 @@
 (map! :mode org-mode
       :nv "C-j" #'org-forward-heading-same-level
       :nv "C-k" #'org-backward-heading-same-level
+      :nv "H" #'evil-org-beginning-of-line
       (:localleader
-        "n" #'org-add-note))
+        "n" #'org-add-note
+        "p" #'org-priority))
