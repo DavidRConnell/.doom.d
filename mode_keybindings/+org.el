d893ab99846434e11806c0e079e9a3e0b326d6f5
David R Connell <davidconnell12@gmail.com>
2019-08-19 16:55:41 -0500

    Fix issue with leaving insert-state

--- mode_keybindings/+org.el
+++ mode_keybindings/+org.el
@@ -4,6 +4,8 @@
       :nv "C-j" #'org-forward-heading-same-level
       :nv "C-k" #'org-backward-heading-same-level
       :nv "H" #'evil-org-beginning-of-line
+      :i "C-[" #'evil-escape-mode
+      :i "ESC" #'evil-escape-mode
       (:localleader
         "n" #'org-add-note
         "p" #'org-priority))
