790db6c3c9d5a0dc03272557c03d87269c3693f9
David R Connell <davidconnell12@gmail.com>
2019-09-18 21:04:41 -0500

    Fix: Matlab recipe

--- packages.el
+++ packages.el
@@ -1,10 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;; (package! matlab-mode :recipe (:fetcher github :repo "yuhonglin/matlab-mode"))
-(package! matlab-mode
-  :recipe (:fetcher git
-           :url "https://git.code.sf.net/p/matlab-emacs/src"
-           :files ("*.el" "*.m"
-                   ("toolbox" "toolbox/*.m")
-                   ("templates" "templates/*.srt"))))
+(package! matlab-emacs :recipe
+  (:fetcher github :repo "ayonga/matlab-emacs"))
