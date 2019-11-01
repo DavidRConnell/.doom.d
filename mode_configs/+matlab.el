31741568662ab70a7cb43407ebfeabcd01b57bed
David R Connell <davidconnell12@gmail.com>
2019-11-01 14:35:13 -0500

    Fix: Change when configs are loaded

--- mode_configs/+matlab.el
+++ mode_configs/+matlab.el
@@ -6,10 +6,20 @@
   (if (equal 0 (buffer-size))
       (insert (concat "function " (substring (buffer-name) 0 -2) "\nend"))))
 
-(add-hook! 'matlab-mode-hook
-  '(display-line-numbers-mode
-    highlight-indent-guides-mode
-    git-gutter-mode
-    matlab-toggle-show-mlint-warnings
-    matlab-toggle-functions-have-end
-    insert-function-snippet))
+(defun fix-matlab-imenu-generic-expression ()
+    (setq imenu-generic-expression
+          '((nil "^\\s-*\\(function\\)\\s-*\\(.*\\)" 2))))
+
+
+(after! matlab-mode
+  (add-hook 'matlab-shell-mode-hook
+            #'company-mode)
+
+  (add-hook! 'matlab-mode-hook
+             #'display-line-numbers-mode
+             #'highlight-indent-guides-mode
+             #'git-gutter-mode
+             #'matlab-toggle-show-mlint-warnings
+             #'matlab-toggle-functions-have-end
+             #'insert-function-snippet
+             #'fix-matlab-imenu-generic-expression))
