9172c23d171e7a41236ec3b78c91d061dcf86e27
David R Connell <davidconnell12@gmail.com>
2019-11-01 14:32:47 -0500

    Clean: Remove recipes

--- packages.el
+++ packages.el
@@ -1,15 +1,12 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-(package! julia-repl :recipe
-  (:fetcher github :repo "tpapp/julia-repl"))
-
-(package! matlab-emacs :recipe
-  (:fetcher github :repo "ayonga/matlab-emacs"))
-
 (package! org-ref)
 (package! org-noter)
 (package! ivy-bibtex)
 (package! counsel-org-clock)
 (package! define-word)
 (package! powerthesaurus)
+(package! matlab-mode)
+(package! julia-repl)
+(package! flycheck-julia)
