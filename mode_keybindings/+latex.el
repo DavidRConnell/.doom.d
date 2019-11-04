7911e8176ae62a2ee2d0d21fbfd95d939c4753d4
David R Connell <davidconnell12@gmail.com>
2019-11-03 19:12:23 -0600

    Add: Latex mode functions

--- mode_keybindings/+latex.el
+++ mode_keybindings/+latex.el
@@ -1,4 +1,10 @@
 ;;; ~/.doom.d/mode_keybindings/+latex.el -*- lexical-binding: t; -*-
 
-(map! :mode latex-mode
-      :nvi "\\t" #'flyspell-correct-word-generic)
+(map! :mode LaTeX-mode
+      :nvi [tab] #'flyspell-correct-word-generic
+      :n "C-SPC" #'flyspell-correct-word-generic
+      (:localleader
+        :desc "Insert figure" "f" #'dc-insert-latex-figure
+        :desc "Insert table" "t" #'dc-insert-latex-table
+        :desc "Insert section" "s" #'dc-insert-latex-section
+        :desc "Insert environment" "e" #'LaTeX-environment))
