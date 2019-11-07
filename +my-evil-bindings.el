eb64b7a562bfeb7705c0122c2363e07a674bf659
David R Connell <davidconnell12@gmail.com>
2019-11-06 20:15:10 -0600

    Whitespace:

--- +my-evil-bindings.el
+++ +my-evil-bindings.el
@@ -53,33 +53,34 @@
         :i [remap newline] #'newline-and-indent  ; auto-indent on newline
         :i "C-j"           #'+default/newline    ; default behavior
 
-      :m  "gs"    #'+evil/easymotion  ; lazy-load `evil-easymotion'
-      (:after evil-easymotion
-        :map evilem-map
-        "SPC" #'avy-goto-char-timer
-        "/" (evilem-create #'evil-ex-search-next
-                           :pre-hook (save-excursion (call-interactively #'swiper))
-                           :bind ((evil-search-wrap)))
-        "?" (evilem-create #'evil-ex-search-previous
-                           :pre-hook (save-excursion (call-interactively #'swiper-backward))
-                           :bind ((evil-search-wrap))))
         :m "r" #'avy-goto-char-in-line
         :m "R" #'avy-goto-char-2
 
-      (:when (featurep! :ui workspaces)
-                :nv "M-t"   #'+workspace/new
-                :nv "M-T"   #'+workspace/display
-                :nv "M-q"    #'+workspace/delete
-                :nv "M-1"   (λ! (+workspace/switch-to 0))
-                :nv "M-2"   (λ! (+workspace/switch-to 1))
-                :nv "M-3"   (λ! (+workspace/switch-to 2))
-                :nv "M-4"   (λ! (+workspace/switch-to 3))
-                :nv "M-5"   (λ! (+workspace/switch-to 4))
-                :nv "M-6"   (λ! (+workspace/switch-to 5))
-                :nv "M-7"   (λ! (+workspace/switch-to 6))
-                :nv "M-8"   (λ! (+workspace/switch-to 7))
-                :nv "M-9"   (λ! (+workspace/switch-to 8))
-                :nv "M-0"   #'+workspace/switch-to-last))
+        :m  "gs"    #'+evil/easymotion  ; lazy-load `evil-easymotion'
+        (:after evil-easymotion
+          :map evilem-map
+          "SPC" #'avy-goto-char-timer
+          "/" (evilem-create #'evil-ex-search-next
+                             :pre-hook (save-excursion (call-interactively #'swiper))
+                             :bind ((evil-search-wrap)))
+          "?" (evilem-create #'evil-ex-search-previous
+                             :pre-hook (save-excursion (call-interactively #'swiper-backward))
+                             :bind ((evil-search-wrap))))
+
+        (:when (featurep! :ui workspaces)
+          :nv "M-t"   #'+workspace/new
+          :nv "M-T"   #'+workspace/display
+          :nv "M-q"    #'+workspace/delete
+          :nv "M-1"   (λ! (+workspace/switch-to 0))
+          :nv "M-2"   (λ! (+workspace/switch-to 1))
+          :nv "M-3"   (λ! (+workspace/switch-to 2))
+          :nv "M-4"   (λ! (+workspace/switch-to 3))
+          :nv "M-5"   (λ! (+workspace/switch-to 4))
+          :nv "M-6"   (λ! (+workspace/switch-to 5))
+          :nv "M-7"   (λ! (+workspace/switch-to 6))
+          :nv "M-8"   (λ! (+workspace/switch-to 7))
+          :nv "M-9"   (λ! (+workspace/switch-to 8))
+          :nv "M-0"   #'+workspace/switch-to-last))
 
 (map! (:when (featurep! :completion company)
         :i "C-SPC"    #'+company/complete
@@ -150,150 +151,150 @@
 
 (defun get-evil-ex-prefix ()
   "Format universal argument for use on evil-ex command state."
-    (cond
-     ((and (evil-visual-state-p)
-           evil-ex-visual-char-range
-           (memq (evil-visual-type) '(inclusive exclusive)))
-      "`<,`>")
-     ((evil-visual-state-p)
-      "'<,'>")
-     (current-prefix-arg
-      (let ((arg (prefix-numeric-value current-prefix-arg)))
-        (cond ((< arg 0) (setq arg (1+ arg)))
-              ((> arg 0) (setq arg (1- arg))))
-        (if (= arg 0) '(".")
-          (format ".,.%+d" arg))))))
+  (cond
+   ((and (evil-visual-state-p)
+         evil-ex-visual-char-range
+         (memq (evil-visual-type) '(inclusive exclusive)))
+    "`<,`>")
+   ((evil-visual-state-p)
+    "'<,'>")
+   (current-prefix-arg
+    (let ((arg (prefix-numeric-value current-prefix-arg)))
+      (cond ((< arg 0) (setq arg (1+ arg)))
+            ((> arg 0) (setq arg (1- arg))))
+      (if (= arg 0) '(".")
+        (format ".,.%+d" arg))))))
 
 (defun get-sp-pair (prefix)
   "Get pair in format for use with sp-rewrap sexp."
-    (--first (equal prefix (car it)) (sp--get-pair-list-context 'wrap)))
+  (--first (equal prefix (car it)) (sp--get-pair-list-context 'wrap)))
 
 (map! :leader
-        "w"    #'save-buffer
-        "q"    #'kill-this-buffer
-        "o"    #'find-file
+      "w"    #'save-buffer
+      "q"    #'kill-this-buffer
+      "o"    #'find-file
 
-        "b"    #'persp-switch-to-buffer
-        "B"    #'switch-to-buffer
-        "+"    #'evil-numbers/inc-at-pt
-        "-"    #'evil-numbers/dec-at-pt
-        ","    #'eshell
+      "b"    #'persp-switch-to-buffer
+      "B"    #'switch-to-buffer
+      "+"    #'evil-numbers/inc-at-pt
+      "-"    #'evil-numbers/dec-at-pt
+      ","    #'eshell
 
-        "s"    #'doom/open-scratch-buffer
-        "!"    #'doom/sudo-this-file
-        ":"    #'eval-expression
-        ";"    #'execute-extended-command
-        "."    #'doom/open-private-config
-        "m"    #'+popup/toggle
-        "/"    #'evil-ex-nohighlight
-        "\\"   #'toggle-truncate-lines
+      "s"    #'doom/open-scratch-buffer
+      "!"    #'doom/sudo-this-file
+      ":"    #'eval-expression
+      ";"    #'execute-extended-command
+      "."    #'doom/open-private-config
+      "m"    #'+popup/toggle
+      "/"    #'evil-ex-nohighlight
+      "\\"   #'toggle-truncate-lines
 
-        :desc "show functions" "l" #'imenu
+      :desc "show functions" "l" #'imenu
 
-        :desc "Surrond With" "["    (lambda! (sp-rewrap-sexp (get-sp-pair "[")))
-        :desc "Surrond With" "{"    (lambda! (sp-rewrap-sexp (get-sp-pair "{")))
-        :desc "Surrond With" "("    (lambda! (sp-rewrap-sexp (get-sp-pair "(")))
-        :desc "Surrond With" "\""   (lambda! (sp-rewrap-sexp (get-sp-pair "\"")))
+      :desc "Surrond With" "["    (lambda! (sp-rewrap-sexp (get-sp-pair "[")))
+      :desc "Surrond With" "{"    (lambda! (sp-rewrap-sexp (get-sp-pair "{")))
+      :desc "Surrond With" "("    (lambda! (sp-rewrap-sexp (get-sp-pair "(")))
+      :desc "Surrond With" "\""   (lambda! (sp-rewrap-sexp (get-sp-pair "\"")))
 
-        (:prefix ("r" . "Replace line")
-          :desc "custom" "c" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<")))
-          :desc "word" "w" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<" (thing-at-point 'word) "\\>/")))
-          :desc "WORD" "W" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<" (thing-at-point 'symbol) "\\>/"))))
+      (:prefix ("r" . "Replace line")
+        :desc "custom" "c" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<")))
+        :desc "word" "w" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<" (thing-at-point 'word) "\\>/")))
+        :desc "WORD" "W" (lambda! (evil-ex (concat (get-evil-ex-prefix) "s/\\<" (thing-at-point 'symbol) "\\>/"))))
 
-        (:prefix ("R" . "Replace buffer")
-          :desc "custom" "c" (lambda! (evil-ex "%s/\\<"))
-          :desc "word" "w" (lambda! (evil-ex (concat "%s/\\<" (thing-at-point 'word) "\\>/")))
-          :desc "WORD" "W" (lambda! (evil-ex (concat "%s/\\<" (thing-at-point 'symbol) "\\>/"))))
+      (:prefix ("R" . "Replace buffer")
+        :desc "custom" "c" (lambda! (evil-ex "%s/\\<"))
+        :desc "word" "w" (lambda! (evil-ex (concat "%s/\\<" (thing-at-point 'word) "\\>/")))
+        :desc "WORD" "W" (lambda! (evil-ex (concat "%s/\\<" (thing-at-point 'symbol) "\\>/"))))
 
-        (:prefix ("p" . "Projects")
-          :desc "open emacs.d" "e" #'+default/browse-emacsd
-          :desc "open notes" "n" (lambda! (doom-project-browse "~/notes/"))
-          :desc "open config" "c" (lambda! (doom-project-browse "~/.config/"))
-          "R" #'projectile-replace
-          "g" #'projectile-grep
-          "o" #'projectile-switch-project
-          "f" #'projectile-find-file
-          "r" #'counsel-buffer-or-recentf)
+      (:prefix ("p" . "Projects")
+        :desc "open emacs.d" "e" #'+default/browse-emacsd
+        :desc "open notes" "n" (lambda! (doom-project-browse "~/notes/"))
+        :desc "open config" "c" (lambda! (doom-project-browse "~/.config/"))
+        "R" #'projectile-replace
+        "g" #'projectile-grep
+        "o" #'projectile-switch-project
+        "f" #'projectile-find-file
+        "r" #'counsel-buffer-or-recentf)
 
-        (:prefix ("c" . "Comments")
-          "l"    #'evil-commentary-line)
+      (:prefix ("c" . "Comments")
+        "l"    #'evil-commentary-line)
 
-        "G" #'magit-dispatch
-        (:prefix ("g" . "git")
-          :desc "Revert" "R" #'vc-revert
-            (:when (featurep! :ui vc-gutter)
-                :desc "Git revert hunk"           "r"   #'git-gutter:revert-hunk
-                :desc "Git stage hunk"            "h"   #'git-gutter:stage-hunk
-                :desc "Git time machine"          "t"   #'git-timemachine-toggle
-                :desc "Jump to next hunk"         "n"   #'git-gutter:next-hunk
-                :desc "Jump to previous hunk"     "N"   #'git-gutter:previous-hunk)
-            (:when (featurep! :tools magit)
-              :desc "status" "s" #'magit-status
-              :desc "add" "a" #'magit-stage-file
-              :desc "add all" "A" #'magit-stage-modified
-              :desc "unstage" "u" #'magit-unstage-file
-              :desc "log" "l" #'magit-log-current
-              :desc "commit" "c" #'magit-commit-create
-              :desc "diff" "d" #'magit-diff-buffer-file
-              :desc "diff worktree" "D" #'magit-diff-working-tree
-              :desc "checkout" "b" #'magit-branch-or-checkout
-              (:prefix ("S" . "stash")
-                :desc "pop" "P" #'magit-stash-pop
-                :desc "push" "p" #'magit-stash-worktree
-                :desc "list" "l" #'magit-stash-list
-                :desc "show" "s" #'magit-stash-show
-                :desc "drop" "d" #'magit-stash-drop
-                :desc "branch" "b" #'magit-stash-branch)))
+      "G" #'magit-dispatch
+      (:prefix ("g" . "git")
+        :desc "Revert" "R" #'vc-revert
+        (:when (featurep! :ui vc-gutter)
+          :desc "Git revert hunk"           "r"   #'git-gutter:revert-hunk
+          :desc "Git stage hunk"            "h"   #'git-gutter:stage-hunk
+          :desc "Git time machine"          "t"   #'git-timemachine-toggle
+          :desc "Jump to next hunk"         "n"   #'git-gutter:next-hunk
+          :desc "Jump to previous hunk"     "N"   #'git-gutter:previous-hunk)
+        (:when (featurep! :tools magit)
+          :desc "status" "s" #'magit-status
+          :desc "add" "a" #'magit-stage-file
+          :desc "add all" "A" #'magit-stage-modified
+          :desc "unstage" "u" #'magit-unstage-file
+          :desc "log" "l" #'magit-log-current
+          :desc "commit" "c" #'magit-commit-create
+          :desc "diff" "d" #'magit-diff-buffer-file
+          :desc "diff worktree" "D" #'magit-diff-working-tree
+          :desc "checkout" "b" #'magit-branch-or-checkout
+          (:prefix ("S" . "stash")
+            :desc "pop" "P" #'magit-stash-pop
+            :desc "push" "p" #'magit-stash-worktree
+            :desc "list" "l" #'magit-stash-list
+            :desc "show" "s" #'magit-stash-show
+            :desc "drop" "d" #'magit-stash-drop
+            :desc "branch" "b" #'magit-stash-branch)))
 
-        (:prefix ("e" . "Email")
-          :desc "Go to mail" "g" #'=mu4e
-          :desc "Compose mail" "c" #'compose-mail-other-window)
+      (:prefix ("e" . "Email")
+        :desc "Go to mail" "g" #'=mu4e
+        :desc "Compose mail" "c" #'compose-mail-other-window)
 
-        (:prefix ("C" . "Code checking")
-          :desc "toggle spell"   "s" #'flyspell-mode
-          :desc "toggle lint"    "c" #'flycheck-mode
-          :desc "list errors"    "l" #'flycheck-list-errors)
+      (:prefix ("C" . "Code checking")
+        :desc "toggle spell"   "s" #'flyspell-mode
+        :desc "toggle lint"    "c" #'flycheck-mode
+        :desc "list errors"    "l" #'flycheck-list-errors)
 
-        (:prefix ("t" . "Org-mode")
-          :desc "Agenda"         "a"  #'org-agenda
-          :desc "Todo list"      "t"  #'org-todo-list
-          :desc "Tags search"    "m"  #'org-tags-view
-          :desc "Org capture"    "x"  #'org-capture
-          :desc "Go to org file" "g"  (lambda!
-                                       (counsel-find-file "~/org/"))
-          :desc "Org store link" "l"  #'org-store-link
-          :desc "View search"    "v"  #'org-search-view
-          (:prefix ("c" . "Clock")
-            :desc "Clock in" "c" #'counsel-org-clock-context
-            :desc "Clock out" "C" #'org-clock-out
-            :desc "Goto last" "g" #'counsel-org-clock-goto))
+      (:prefix ("t" . "Org-mode")
+        :desc "Agenda"         "a"  #'org-agenda
+        :desc "Todo list"      "t"  #'org-todo-list
+        :desc "Tags search"    "m"  #'org-tags-view
+        :desc "Org capture"    "x"  #'org-capture
+        :desc "Go to org file" "g"  (lambda!
+                                     (counsel-find-file "~/org/"))
+        :desc "Org store link" "l"  #'org-store-link
+        :desc "View search"    "v"  #'org-search-view
+        (:prefix ("c" . "Clock")
+          :desc "Clock in" "c" #'counsel-org-clock-context
+          :desc "Clock out" "C" #'org-clock-out
+          :desc "Goto last" "g" #'counsel-org-clock-goto))
 
-        (:prefix ("n" . "Noter")
-          :desc "Run Noter" "n"    #'org-noter
-          :desc "Open Index" "i"  (lambda! (find-file refs-bib))
-          :desc "Open Reference" "r" (lambda! (counsel-find-file refs-pdfs))
-          :desc "Open Notes" "o" (lambda! (find-file refs-notes))
-          :desc "Insert Cite" "c" #'org-ref-insert-cite-with-completion)
+      (:prefix ("n" . "Noter")
+        :desc "Run Noter" "n"    #'org-noter
+        :desc "Open Index" "i"  (lambda! (find-file refs-bib))
+        :desc "Open Reference" "r" (lambda! (counsel-find-file refs-pdfs))
+        :desc "Open Notes" "o" (lambda! (find-file refs-notes))
+        :desc "Insert Cite" "c" #'org-ref-insert-cite-with-completion)
 
-        (:prefix ("d" . "Dictionary")
-          :desc "Define at point" "d" #'define-word-at-point
-          :desc "Define any word" "D" #'define-word
-          :desc "Thesaurus" "t" #'powerthesaurus-lookup-word-dwim))
+      (:prefix ("d" . "Dictionary")
+        :desc "Define at point" "d" #'define-word-at-point
+        :desc "Define any word" "D" #'define-word
+        :desc "Thesaurus" "t" #'powerthesaurus-lookup-word-dwim))
 
-  ;; Minibuffer
-  (define-key! evil-ex-completion-map
-    "C-a" #'move-beginning-of-line
-    "C-b" #'backward-word
-    "C-SPC" (if (featurep! :completion ivy)
+;; Minibuffer
+(define-key! evil-ex-completion-map
+  "C-a" #'move-beginning-of-line
+  "C-b" #'backward-word
+  "C-SPC" (if (featurep! :completion ivy)
               #'counsel-minibuffer-history
             #'helm-minibuffer-history))
 
-  (define-key! :keymaps +default-minibuffer-maps
-    "C-v"    #'yank
-    "C-z"    (λ! (ignore-errors (call-interactively #'undo)))
-    "C-a"    #'move-beginning-of-line
-    "C-b"    #'backward-word
-    "C-r"    #'evil-paste-from-register
-    ;; Scrolling lines
-    "C-n"    #'next-line
-    "C-p"    #'previous-line)
+(define-key! :keymaps +default-minibuffer-maps
+  "C-v"    #'yank
+  "C-z"    (λ! (ignore-errors (call-interactively #'undo)))
+  "C-a"    #'move-beginning-of-line
+  "C-b"    #'backward-word
+  "C-r"    #'evil-paste-from-register
+  ;; Scrolling lines
+  "C-n"    #'next-line
+  "C-p"    #'previous-line)
