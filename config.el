9ebaf778f3ac63a658ab2f60ec70141ca836483b
David R Connell <davidconnell12@gmail.com>
2019-11-06 20:37:25 -0600

    Fix: Git timemachine setup

--- config.el
+++ config.el
@@ -5,11 +5,12 @@
 (if (featurep 'evil)
     (load! "+my-evil-bindings"))
 
-(add-hook 'git-timemachine-mode-hook
-          #'git-timemachine-show-revision-fuzzy
-          (map! :mode git-timemachine-mode
-                :ni "C-p" #'git-timemachine-show-previous-revision
-                :ni "C-n" #'git-timemachine-show-next-revision))
+(after! git-timemachine
+  (add-hook 'git-timemachine-mode-hook
+            #'git-timemachine-show-revision-fuzzy)
+  (map! :map git-timemachine-mode-map
+        "C-p" #'git-timemachine-show-previous-revision
+        "C-n" #'git-timemachine-show-next-revision))
 
 (after! term
   (add-hook 'term-mode-hook
@@ -17,7 +18,7 @@
 
 (after! julia-mode
   (add-hook! 'julia-mode-hook
-            'julia-repl-mode))
+             'julia-repl-mode))
 
 (after! evil-magit
   (map! :mode magit-mode
