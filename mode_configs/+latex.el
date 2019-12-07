811d7f1b59bc17243e22637a7ec8081e5516ba6d
David R Connell <davidconnell12@gmail.com>
2019-12-06 22:08:59 -0600

    Add: Set up use of syntex package

--- mode_configs/+latex.el
+++ mode_configs/+latex.el
@@ -1,9 +1,8 @@
 ;;; ~/.doom.d/mode_configs/+latex.el -*- lexical-binding: t; -*-
 
 (after! latex
+  (setq-default TeX-engine 'xetex)
   (setq TeX-source-correlate-mode nil)
-  (load "~/projects/syntex/syntex.el"))
-
-(add-hook! 'LaTeX-mode-hook
-           #'outline-minor-mode
-           (lambda () (TeX-engine-set 'xetex)))
+  (load "~/projects/syntex/syntex.el")
+  (add-hook! 'LaTeX-mode-hook
+            #'outline-minor-mode))
--- mode_keybindings/+latex.el
+++ mode_keybindings/+latex.el
@@ -1,10 +1,17 @@
 ;;; ~/.doom.d/mode_keybindings/+latex.el -*- lexical-binding: t; -*-
 
-(map! :mode LaTeX-mode
-      :nvi [tab] #'flyspell-correct-word-generic
-      :n "C-SPC" #'flyspell-correct-word-generic
-      (:localleader
-        :desc "Insert figure" "f" #'dc-insert-latex-figure
-        :desc "Insert table" "t" #'dc-insert-latex-table
-        :desc "Insert section" "s" #'dc-insert-latex-section
-        :desc "Insert environment" "e" #'LaTeX-environment))
+(after! latex
+  (map! :mode LaTeX-mode
+        :i [tab] #'flyspell-correct-word-generic
+        (:localleader
+          :desc "Insert figure" "f" #'syntex-insert-figure
+          :desc "Insert subfigures" "F" #'syntex-insert-subfigure
+          :desc "Insert table" "t" #'syntex-insert-table
+          :desc "Insert section" "s" #'syntex-insert-section
+          :desc "Insert environment" "e" #'LaTeX-environment
+          :desc "Cite" "c" #'syntex-cite
+          :desc "Bold" "B" #'syntex-insert-bold
+          :desc "Emphasize" "b" #'syntex-insert-emph
+          :desc "Italicize" "i" #'syntex-insert-italic
+          :desc "label" "l" #'syntex-insert-label
+          :desc "Go to main" "m" #'syntex-goto-main)))
