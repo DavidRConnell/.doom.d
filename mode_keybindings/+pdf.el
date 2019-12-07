0502e5df29c13975f43f9cc29d05ab7ecb5e4ccc
David R Connell <davidconnell12@gmail.com>
2019-12-06 22:12:49 -0600

    Add: Custom pdf outline
    Default displays title for all entries making it difficult to read

--- mode_keybindings/+pdf.el
+++ mode_keybindings/+pdf.el
@@ -7,5 +7,14 @@
       :ni "j" #'pdf-view-next-line-or-next-page
       :ni "k" #'pdf-view-previous-line-or-previous-page)
 
+(after! pdf-tools
+  (defun dc-pdf-outline-imenu-slim ()
+    (interactive)
+    (let* ((outline (pdf-outline-imenu-create-index-flat))
+           (key (completing-read
+                 "Outline: "
+                 (mapcar 'car outline)
+                 nil t nil nil)))
+      (imenu (assoc key outline)))))
 (provide '+pdf)
 ;;; +pdf.el ends here
