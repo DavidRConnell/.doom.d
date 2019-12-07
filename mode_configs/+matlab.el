ae35499e0ae9c7cfa8c89d8bf290aed1b51a7594
David R Connell <davidconnell12@gmail.com>
2019-12-06 22:16:11 -0600

    Add: Company and flycheck support

--- mode_configs/+matlab.el
+++ mode_configs/+matlab.el
@@ -1,6 +1,3 @@
-(setq matlab-shell-command "/usr/local/MATLAB/bin/matlab")
-(setq matlab-shell-command-switches (list "-nodesktop" "-nosplash"))
-
 (defun insert-function-snippet ()
   "Add snippet for matlab function when opening a new .m file."
   (if (equal 0 (buffer-size))
@@ -10,16 +7,22 @@
     (setq imenu-generic-expression
           '((nil "^\\s-*\\(function\\)\\s-*\\(.*\\)" 2))))
 
+(after! matlab
+  (setq matlab-shell-command "/usr/local/MATLAB/bin/matlab")
+  (setq matlab-shell-command-switches (list "-nodesktop" "-nosplash"))
+  (load (concat doom-private-dir "extras/+company-matlab.el"))
+  (load (concat doom-private-dir "extras/+flycheck-matlab-mlint.el"))
+  (setq flycheck-matlab-mlint-executable
+        "/usr/local/MATLAB/bin/glnxa64/mlint")
 
-(after! matlab-mode
   (add-hook 'matlab-shell-mode-hook
             #'company-mode)
 
   (add-hook! 'matlab-mode-hook
-             #'display-line-numbers-mode
-             #'highlight-indent-guides-mode
-             #'git-gutter-mode
-             #'matlab-toggle-show-mlint-warnings
-             #'matlab-toggle-functions-have-end
-             #'insert-function-snippet
-             #'fix-matlab-imenu-generic-expression))
+              #'display-line-numbers-mode
+              #'highlight-indent-guides-mode
+              #'git-gutter-mode
+              #'matlab-toggle-functions-have-end
+              #'insert-function-snippet
+              #'fix-matlab-imenu-generic-expression
+              #'yas-minor-mode))
