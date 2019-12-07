d0f5b422d8e72f638e58b61580272a89141c3f3f
David R Connell <davidconnell12@gmail.com>
2019-12-06 22:11:07 -0600

    Remove: No longer used notmuch config

--- mode_configs/+notmuch.el
+++
@@ -1,5 +0,0 @@
-;;; +notmuch.el --- description -*- lexical-binding: t; -*-
-
-(setq +notmuch-sync-backend 'offlineimap)
-(setq +notmuch-mail-folder "~/mail/")
-(autoload 'notmuch "notmuch" "notmuch mail" t)
