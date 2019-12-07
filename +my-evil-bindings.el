c709c50eaad02c2d79867ae05a8bf61a21d379d3
David R Connell <davidconnell12@gmail.com>
2019-12-06 21:55:56 -0600

    Update: Copy over changes from default evil-bindings

--- +my-evil-bindings.el
+++ +my-evil-bindings.el
@@ -1,8 +1,5 @@
 ;;; +my_evil_bindings.el --- Port of my init.vim -*- lexical-binding: t; -*-
 ;;;
-;;; Commentary:
-;;;             Things TODO main keybindings
-;;;                         Stop auto delete (smart-parens)
 
 (setq evil-collection-key-blacklist
       ;;; Code:
@@ -10,93 +7,169 @@
             doom-leader-key doom-localleader-key
             doom-leader-alt-key doom-localleader-alt-key))
 
+(when (featurep! :editor evil +everywhere)
+  ;; Have C-u behave similarly to `doom/backward-to-bol-or-indent'.
+  ;; NOTE SPC u replaces C-u as the universal argument.
+  (map! :i "C-u" #'doom/backward-kill-to-bol-and-indent
+        :i "C-w" #'backward-kill-word
+        ;; Vimmish ex motion keys
+        :i "C-b" #'backward-word
+        :i "C-f" #'forward-word)
+
+  ;; Minibuffer
+  (define-key! evil-ex-completion-map
+    "C-a" #'move-beginning-of-line
+    "C-b" #'backward-word
+    "C-SPC" (if (featurep! :completion ivy)
+                #'counsel-minibuffer-history
+              #'helm-minibuffer-history))
+
+  (define-key! :keymaps +default-minibuffer-maps
+    [escape] #'abort-recursive-edit
+    "C-a"    #'move-beginning-of-line
+    "C-b"    #'backward-word
+    "C-f"    #'forward-word
+    "C-r"    #'evil-paste-from-register
+    "C-u"    #'doom/backward-kill-to-bol-and-indent
+    "C-v"    #'yank
+    "C-w"    #'backward-kill-word
+    "C-z"    (λ! (ignore-errors (call-interactively #'undo)))
+    ;; Scrolling lines
+    "C-n"    #'next-line
+    "C-p"    #'previous-line
+    "C-j"  #'scroll-up-command
+    "C-k"  #'scroll-down-command)
+
+  (define-key! read-expression-map
+    "C-n" #'next-line-or-history-element
+    "C-p" #'previous-line-or-history-element))
+
+;;
+;;; Global evil keybinds
 (map!
-        ;; Global evil keybinds
-        :m  "]a"    #'evil-forward-arg
-        :m  "[a"    #'evil-backward-arg
-        :m  "]o"    #'outline-next-visible-heading
-        :m  "[o"    #'outline-previous-visible-heading
-        :n  "}"     #'next-buffer
-        :n  "{"     #'previous-buffer
-        :n  "zx"    #'kill-this-buffer
-        :n  "ZX"    #'bury-buffer
-        :n  "gp"    #'+evil/reselect-paste
-        :n  "g="    #'widen
-        :v  "g="    #'+evil:narrow-buffer
-        :nv "z="    #'flyspell-correct-word-generic
-        :nv "g@"    #'+evil:apply-macro
-        :nv "C-q"    #'+evil:apply-macro
-        :nv "Q"     #'evil-execute-macro
-        :nv "gx"    #'evil-exchange
-        :nv "gd"    #'dumb-jump-go
-        :nv "gc"    #'evilnc-comment-operator
-        :nv "gr"    #'dc-apply-ex-substitute-word
-        :nv "gR"    #'dc-apply-ex-substitute-WORD
-        :nv "gk"    #'dc-apply-ex-substitute-custom
-        :nv "C-a"   #'evil-numbers/inc-at-pt
-        :nv "C-S-a" #'evil-numbers/dec-at-pt
-        :v  "gp"    #'+evil/paste-preserve-register
-        :v  "@"     #'+evil:apply-macro
-        :v  "."     #'+evil:apply-macro
-        :v  "<"     #'+evil/visual-dedent
-        :v  ">"     #'+evil/visual-indent
-        :nv "j"     #'evil-next-visual-line
-        :nv "k"     #'evil-previous-visual-line
-        :nv "H"     #'evil-first-non-blank
-        :nv "L"     #'evil-end-of-line
-        :nv "K"     #'evil-scroll-line-up
-        :nv "J"     #'evil-scroll-line-down
-        :n "C-j"    #'evil-scroll-page-down
-        :n "C-k"    #'evil-scroll-page-up
-        :v "C-j"    #'avy-goto-line-below
-        :v "C-k"    #'avy-goto-line-above
-        :nv "C-m"   #'evil-goto-mark
-        :nv "ZZ"    #'save-buffers-kill-terminal
-        :nv "/" #'swiper
-        :nv "?" #'swiper-backward
-        :nv "gn" #'swiper-thing-at-point
-        :nvi "M-/" #'ace-link
+ :i [tab] (general-predicate-dispatch nil ; fall back to nearest keymap
+            (and (featurep! :editor snippets)
+                 (bound-and-true-p yas-minor-mode)
+                 (yas-maybe-expand-abbrev-key-filter 'yas-expand))
+            #'yas-expand
+            (and (featurep! :completion company +tng)
+                 (+company-has-completion-p))
+            #'+company/complete)
+ :n [tab] (general-predicate-dispatch nil
+            (and (featurep! :editor fold)
+                 (save-excursion (end-of-line) (invisible-p (point))))
+            #'+fold/toggle
+            (fboundp 'evil-jump-item)
+            #'evil-jump-item)
+ :v [tab] (general-predicate-dispatch nil
+            (and (bound-and-true-p yas-minor-mode)
+                 (or (eq evil-visual-selection 'line)
+                     (not (memq (char-after) (list ?\( ?\[ ?\{ ?\} ?\] ?\))))))
+            #'yas-insert-snippet
+            (fboundp 'evil-jump-item)
+            #'evil-jump-item)
 
-        (:prefix "C-h"
-          :desc "Find info manual" "RET" #'info-display-manual
-          :desc "Top layer key map" "K" #'which-key-show-top-level)
+ ;; Smarter newlines
+ :i [remap newline] #'newline-and-indent  ; auto-indent on newline
+ :i "C-j"           #'+default/newline    ; default behavior
 
-        ;; Smarter newlines
-        :i [remap newline] #'newline-and-indent  ; auto-indent on newline
-        :i "C-j"           #'+default/newline    ; default behavior
+ (:after help :map help-mode-map
+   :n "o"       #'ace-link-help)
+ (:after helpful :map helpful-mode-map
+   :n "o"       #'ace-link-help)
+ (:after info :map Info-mode-map
+   :n "o"       #'ace-link-info)
+ (:after apropos :map apropos-mode-map
+   :n "o"       #'ace-link-help
+   :n "TAB"     #'forward-button
+   :n [tab]     #'forward-button
+   :n [backtab] #'backward-button)
+ (:after view :map view-mode-map
+   [escape]  #'View-quit-all)
+ (:after man :map Man-mode-map
+   :n "q"    #'kill-current-buffer)
 
-        ; Use C-s (snipe) instead of C-t becaulse C-t is my stumpwm key
-        :nvm "C-s" (lambda! ; make exclusive (more like till than from)
-                   (call-interactively #'avy-goto-char-in-line)
-                   (point))
+ (:prefix "C-h"
+   :desc "Find info manual" "RET" #'info-display-manual
+   :desc "Top layer key map" "K" #'which-key-show-top-level)
 
-        :nvm "C-f" #'avy-goto-char-in-line
+ :m  "]a"    #'evil-forward-arg
+ :m  "[a"    #'evil-backward-arg
+ :m  "]o"    #'outline-next-visible-heading
+ :m  "[o"    #'outline-previous-visible-heading
+ :n  "}"     #'next-buffer
+ :n  "{"     #'previous-buffer
+ :n  "zx"    #'kill-this-buffer
+ :n  "ZX"    #'bury-buffer
+ :n  "gp"    #'+evil/reselect-paste
+ :n  "g="    #'widen
+ :v  "g="    #'+evil:narrow-buffer
+ :nv "z="    #'flyspell-correct-word-generic
+ :nv "g@"    #'+evil:apply-macro
+ :nv "C-q"    #'+evil:apply-macro
+ :nv "Q"     #'evil-execute-macro
+ :nv "gx"    #'evil-exchange
+ :nv "gK"    #'+lookup/documentation
+ :nv "gc"    #'dc-comment-line-operator
+ :nv "gr"    #'dc-apply-ex-substitute-word
+ :nv "gR"    #'dc-apply-ex-substitute-WORD
+ :nv "gk"    #'dc-apply-ex-substitute-custom
+ :nv "C-a"   #'evil-numbers/inc-at-pt
+ :nv "C-S-a" #'evil-numbers/dec-at-pt
+ :v  "gp"    #'+evil/paste-preserve-register
+ :v  "@"     #'+evil:apply-macro
+ :v  "."     #'+evil:apply-macro
+ :v  "<"     #'+evil/visual-dedent
+ :v  ">"     #'+evil/visual-indent
+ :nv "j"     #'evil-next-visual-line
+ :nv "k"     #'evil-previous-visual-line
+ :nv "H"     #'evil-first-non-blank
+ :nv "L"     #'evil-end-of-line
+ :nv "K"     #'evil-scroll-line-up
+ :nv "J"     #'evil-scroll-line-down
+ :n "C-j"    #'evil-scroll-page-down
+ :n "C-k"    #'evil-scroll-page-up
+ :v "C-j"    #'avy-goto-line-below
+ :v "C-k"    #'avy-goto-line-above
+ :nv "C-m"   #'evil-goto-mark
+ :nv "ZZ"    #'save-buffers-kill-terminal
+ :nv "/" #'swiper
+ :nv "?" #'swiper-backward
+ :nv "gn" #'swiper-thing-at-point
+ :nvi "M-/" #'ace-link
+ :nvi "C-u" #'universal-argument
 
-        :nv "C-/" #'avy-goto-char-timer
-        :m  "C-e"    #'+evil/easymotion  ; lazy-load `evil-easymotion'
-        (:after evil-easymotion
-          :map evilem-map
-          "/" (evilem-create #'evil-ex-search-next
-                             :pre-hook (save-excursion (call-interactively #'swiper))
-                             :bind ((evil-search-wrap)))
-          "?" (evilem-create #'evil-ex-search-previous
-                             :pre-hook (save-excursion (call-interactively #'swiper-backward))
-                             :bind ((evil-search-wrap))))
+ ;; Use C-s (snipe) instead of C-t becaulse C-t is my stumpwm key
+ :nvm "C-s" (lambda! ; make exclusive (more like till than from)
+             (call-interactively #'avy-goto-char-in-line)
+             (point))
 
-        (:when (featurep! :ui workspaces)
-          :nv "M-t"   #'+workspace/new
-          :nv "M-T"   #'+workspace/display
-          :nv "M-q"    #'+workspace/delete
-          :nv "M-1"   (λ! (+workspace/switch-to 0))
-          :nv "M-2"   (λ! (+workspace/switch-to 1))
-          :nv "M-3"   (λ! (+workspace/switch-to 2))
-          :nv "M-4"   (λ! (+workspace/switch-to 3))
-          :nv "M-5"   (λ! (+workspace/switch-to 4))
-          :nv "M-6"   (λ! (+workspace/switch-to 5))
-          :nv "M-7"   (λ! (+workspace/switch-to 6))
-          :nv "M-8"   (λ! (+workspace/switch-to 7))
-          :nv "M-9"   (λ! (+workspace/switch-to 8))
-          :nv "M-0"   #'+workspace/switch-to-last))
+ :nvm "C-f" #'avy-goto-char-in-line
+
+ :nv "C-/" #'avy-goto-char-timer
+ (:after evil-easymotion
+   :map evilem-map
+   "/" (evilem-create #'evil-ex-search-next
+                      :pre-hook (save-excursion (call-interactively #'swiper))
+                      :bind ((evil-search-wrap)))
+   "?" (evilem-create #'evil-ex-search-previous
+                      :pre-hook (save-excursion (call-interactively #'swiper-backward))
+                      :bind ((evil-search-wrap))))
+
+ (:when (featurep! :ui workspaces)
+   :nv "M-t"   #'+workspace/new
+   :nv "M-T"   #'+workspace/display
+   :nv "M-q"   #'+workspace/delete
+   :nv "M-1"   (λ! (+workspace/switch-to 0))
+   :nv "M-2"   (λ! (+workspace/switch-to 1))
+   :nv "M-3"   (λ! (+workspace/switch-to 2))
+   :nv "M-4"   (λ! (+workspace/switch-to 3))
+   :nv "M-5"   (λ! (+workspace/switch-to 4))
+   :nv "M-6"   (λ! (+workspace/switch-to 5))
+   :nv "M-7"   (λ! (+workspace/switch-to 6))
+   :nv "M-8"   (λ! (+workspace/switch-to 7))
+   :nv "M-9"   (λ! (+workspace/switch-to 8))
+   :nv "M-0"   #'+workspace/switch-to-final))
 
 (map! (:when (featurep! :completion company)
         :i "C-SPC"    #'+company/complete
@@ -291,25 +364,7 @@
         :desc "Insert Cite" "c" #'org-ref-insert-cite-with-completion)
 
       (:prefix ("d" . "Dictionary")
-        :desc "Define at point" "d" #'define-word-at-point
-        :desc "Define any word" "D" #'define-word
-        :desc "Thesaurus" "t" #'powerthesaurus-lookup-word-dwim
-        :desc "Search with wordnut" #'wordnut-lookup-current-word))
-
-;; Minibuffer
-(define-key! evil-ex-completion-map
-  "C-a" #'move-beginning-of-line
-  "C-b" #'backward-word
-  "C-SPC" (if (featurep! :completion ivy)
-              #'counsel-minibuffer-history
-            #'helm-minibuffer-history))
-
-(define-key! :keymaps +default-minibuffer-maps
-  "C-v"    #'yank
-  "C-z"    (λ! (ignore-errors (call-interactively #'undo)))
-  "C-a"    #'move-beginning-of-line
-  "C-b"    #'backward-word
-  "C-r"    #'evil-paste-from-register
-  ;; Scrolling lines
-  "C-n"    #'next-line
-  "C-p"    #'previous-line)
+        :desc "Define at point"     "d" #'define-word-at-point
+        :desc "Define any word"     "D" #'define-word
+        :desc "Thesaurus"           "t" #'powerthesaurus-lookup-word-dwim
+        :desc "Search with wordnut" "w" #'wordnut-lookup-current-word))
