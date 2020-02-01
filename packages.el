95d216c06444507cf41d55b1b3b9b914936a2c43
David R Connell <davidconnell12@gmail.com>
2020-01-31 21:55:35 -0600

    Add: Packages

--- packages.el
+++ packages.el
@@ -7,9 +7,16 @@
 (package! counsel-org-clock)
 (package! define-word)
 (package! powerthesaurus)
+(package! evil-matchit)
 (package! scad-mode)
 (package! matlab-mode)
 (package! flycheck-julia)
+(package! ripgrep)
+(package! ace-link)
+(package! function-args)
+
+(package! evil-matchit-matlab :recipe
+  (:local-repo "~/projects/evil-matchit-matlab"))
 
 (package! evil-snipe :disable t)
 (package! writegood-mode :disable t)
