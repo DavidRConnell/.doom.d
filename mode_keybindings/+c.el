7a9a93de7cd9a24d8e59a50c5868c72c9b841c4c
David R Connell <davidconnell12@gmail.com>
2020-01-31 22:10:22 -0600

    Add: New package functions and others

--- mode_keybindings/+c.el
+++ mode_keybindings/+c.el
@@ -1,4 +1,11 @@
 (map! :mode c-mode
       (:localleader
         "r" #'quickrun
-        "R" #'quickrun-shell))
+        "R" #'quickrun-shell
+        "c" (lambda! (compile (concat  "make run FILENAME="
+                                         (file-name-sans-extension (buffer-name))
+                                         " && make clean >/dev/null")))
+        "d" (lambda! (gdb (concat "gdb -i=mi "
+                                  (file-name-sans-extension (buffer-name)))))
+        "l" #'moo-jump-local
+        "L" #'moo-jump-directory))
