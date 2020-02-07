6a022eae257900b48ebceee2c6bff07250e8c663
David R Connell <davidconnell12@gmail.com>
2020-02-06 19:15:28 -0600

    Fix: Update keybindings to reflect syntex updates

--- mode_keybindings/+latex.el
+++ mode_keybindings/+latex.el
@@ -10,8 +10,11 @@
           :desc "Insert section" "s" #'syntex-insert-section
           :desc "Insert environment" "e" #'LaTeX-environment
           :desc "Cite" "c" #'syntex-cite
-          :desc "Bold" "B" #'syntex-insert-bold
+          :desc "Bold" "B" #'syntex-insert-textbf
           :desc "Emphasize" "b" #'syntex-insert-emph
-          :desc "Italicize" "i" #'syntex-insert-italic
+          :desc "Italicize" "i" #'syntex-insert-textit
           :desc "label" "l" #'syntex-insert-label
-          :desc "Go to main" "m" #'syntex-goto-main)))
+          :desc "reference" "r" #'syntex-insert-ref
+          :desc "Go to main" "m" #'syntex-goto-main)
+        (:prefix "C-c"
+          :desc "Build project" "C-c" #'syntex-build-project)))
