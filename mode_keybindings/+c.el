01436fab3dd1956b5d78a42ea984109405bf7fd8
David R Connell <davidconnell12@gmail.com>
2020-02-18 11:24:23 -0600

    Add: toggle between c and header files

--- mode_keybindings/+c.el
+++ mode_keybindings/+c.el
@@ -9,4 +9,15 @@
         :desc "Debug" "d" (lambda! (gdb (concat "gdb -i=mi "
                                           (file-name-sans-extension (buffer-name)))))
         "l" #'moo-jump-local
-        "L" #'moo-jump-directory))
+        "L" #'moo-jump-directory
+        "h" #'dc-toggle-header-file))
+
+(defun dc-toggle-header-file ()
+  (interactive)
+  (let* ((fullname (buffer-name))
+         (ext (file-name-extension fullname))
+         (name (file-name-sans-extension fullname)))
+    (cond ((and (string= ext "c") (file-exists-p (concat name ".h")))
+           (find-file (concat name ".h")))
+          ((and (string= ext "h") (file-exists-p (concat name ".c")))
+           (find-file (concat name ".c"))))))
