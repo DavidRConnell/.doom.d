8b8f5786d3ab3eb7a186a03decdc3401f9bc3587
David R Connell <davidconnell12@gmail.com>
2020-02-18 11:24:03 -0600

    Add: descriptions

--- mode_keybindings/+c.el
+++ mode_keybindings/+c.el
@@ -1,11 +1,12 @@
-(map! :mode c-mode
+(map! :mode (c-mode objc-mode)
       (:localleader
         "r" #'quickrun
         "R" #'quickrun-shell
-        "c" (lambda! (compile (concat  "make run FILENAME="
+        :desc "Compile" "c" (lambda! (compile
+                                (concat  "make run FILENAME="
                                          (file-name-sans-extension (buffer-name))
                                          " && make clean >/dev/null")))
-        "d" (lambda! (gdb (concat "gdb -i=mi "
-                                  (file-name-sans-extension (buffer-name)))))
+        :desc "Debug" "d" (lambda! (gdb (concat "gdb -i=mi "
+                                          (file-name-sans-extension (buffer-name)))))
         "l" #'moo-jump-local
         "L" #'moo-jump-directory))
