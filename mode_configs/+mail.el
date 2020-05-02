56ff6ae7823c4ae907dd161ebbbc3bcfe0f6027b
David R Connell <davidconnell12@gmail.com>
2020-05-02 15:49:50 -0500

    Clean: re due mail

--- mode_configs/+mail.el
+++ mode_configs/+mail.el
@@ -1,25 +1,42 @@
 ;;; ~/.doom.d/mode_configs/+mail.el -*- lexical-binding: t; -*-
 
-(setq mu4e-maildir "~/.mail/gmail/")
-(setq mail-user-agent 'message-user-agent)
-(setq user-mail-address "davidconnell12@gmail.com"
-      user-full-name "David R. Connell")
+;; (setq mu4e-maildir "~/.mail/")
+;; (setq mail-user-agent 'message-user-agent)
+
+;; (setq message-send-mail-function 'smtpmail-send-it
+;;       smtpmail-smtp-server "smtp.gmail.com"
+;;       smtpmail-stream-type 'ssl
+;;       smtpmail-smtp-service 465)
+
+;; (setq smtpmail-debug-info t)
+;; (setq message-auto-save-directory "~/News/drafts")
+;; (setq message-kill-buffer-on-exit t)
+;; (setq message-directory "~/News/")
+
+;; (setq shr-color-visible-luminance-min 80)
+;; (setq message-signature
+;;       "Thank you\nDavid R. Connell")
+
+;; (add-hook! 'mu4e-compose-mode-hook
+;;   (turn-off-auto-fill)
+;;   (visual-line-mode))
+
+(setq message-default-headers "Cc: \nBcc: \n")
+(set-email-account! "gmail"
+                    '((user-full-name         . "David R. Connell")
+                      (user-mail-address      . "davidconnell12@gmail.com")
+                      (smtpmail-smtp-user     . "davidconnell12@gmail.com")
+                      (mu4e-sent-folder       . "/gmail/Sent")
+                      (mu4e-drafts-folder     . "/gmail/Drafts")
+                      (mu4e-trash-folder      . "/gmail/Trash")
+                      (mu4e-refile-folder     . "/gmail/All Mail")
+                      (mu4e-compose-signature . "Thank you\nDavid R. Connell"))
+                    t)
 
 (setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "davidconnell12@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-stream-type 'ssl
-      smtpmail-smtp-service 465)
-
-(setq smtpmail-debug-info t)
-(setq message-default-headers "Cc: \nBcc: \n")
-(setq message-auto-save-directory "~/.mail/gmail/drafts")
-(setq message-kill-buffer-on-exit t)
-(setq message-directory "~/.mail/gmail")
-
-(setq shr-color-visible-luminance-min 80)
-(setq message-signature
-      "Thank you\nDavid R. Connell")
-
-(add-hook! 'message-mode-hook
-  (turn-off-auto-fill)
-  (visual-line-mode))
+      smtpmail-smtp-service 587
+      smtpmail-local-domain "gmail.com")
