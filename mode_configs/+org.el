63d5b0a6493702820faa50a1ff88414885e25111
David R Connell <davidconnell12@gmail.com>
2020-06-05 17:01:46 -0500

    Add: fix octave ob
    To work more like other languages.

--- mode_configs/+org.el
+++ mode_configs/+org.el
@@ -267,6 +267,9 @@ right sequence."
 
 (add-to-list 'org-file-apps '("\\.pdf\\'" . (lambda (_file link)
                                               (call-process "xdg-open" nil 0 nil link))))
+(after! ob-octave
+  (load (concat doom-private-dir "extras/+ob-octave-fix.el")))
+
 (after! org-ref
   (org-ref-ivy-cite-completion)
   (setq org-ref-completion-library 'org-ref-ivy-cite)
