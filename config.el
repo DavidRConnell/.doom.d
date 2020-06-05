ba924d38b9049cb4ed9b46ec9b5b3a4f9b6f3c95
David R Connell <davidconnell12@gmail.com>
2020-06-05 16:51:31 -0500

    Change: deft file name to work better with roam

--- config.el
+++ config.el
@@ -103,6 +103,8 @@
 
 (after! deft
   (setq deft-extensions '("org"))
+  (setq deft-new-file-format "%Y%m%d%H%M%S")
+  (setq deft-use-filter-string-for-filename nil)
   (map! :map 'deft-mode-map
         :i "C-o" #'deft-open-file-other-window
         :i "C-w" #'deft-filter-decrement-word
