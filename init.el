15da6b8c3b90962b1c9c02b8be250926b11cdcba
David R Connell <davidconnell12@gmail.com>
2020-07-26 11:25:13 -0500

    Remove: spell everywhere
    - It was distracting and possibly the reason for causing emacs to stall
    in some files.
    - Remove everywhere module flag so spell is only used in text files.
    - Spelling is not checked in all code files anymore. Add spell-fu mode
    to programming modes as needed.

--- init.el
+++ init.el
@@ -57,8 +57,7 @@
        :checkers
        (syntax           ; tasing you for every semicolon you forget
         +childframe)
-       (spell            ; tasing you for misspelling mispelling
-        +everywhere)
+       spell             ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
